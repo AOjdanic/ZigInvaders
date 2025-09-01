449313a5587003641da83c2cfb4ce105fb8ff1a0
Aleksa Ojdanic <aojdanic97@gmail.com>
2025-09-02 00:24:25 +0200

    start implementing the main game loop

--- src/main.zig
+++ src/main.zig
@@ -2,26 +2,18 @@ const std = @import("std");
 const ZigInvaders = @import("ZigInvaders");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try ZigInvaders.bufferedPrint();
-}
+    var game_over = false;
+    var stdin_buffer: [1024]u8 = undefined;
+    var stdin_reader = std.fs.File.stdin().reader(&stdin_buffer);
+    const stdin = &stdin_reader.interface;
+    var input_char: u8 = undefined;
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+    while (!game_over) {
+        input_char = try stdin.takeByte();
+        if (input_char == 'q') {
+            game_over = true;
         }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+
+        std.debug.print("you typed: {c}\n", .{input_char});
+    }
 }
